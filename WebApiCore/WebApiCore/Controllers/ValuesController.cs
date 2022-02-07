a3a8f8868cb9720fb03dc79b72d20ac629fdd014
Randive <2063457@cognizant.com>
2022-02-07 17:05:55 +0530

    Adding web api core project

---
+++ WebApiCore/WebApiCore/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/Values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Dhanashri", "Randive" };
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "Hello";
+        }
+
+       
+    }
+}
